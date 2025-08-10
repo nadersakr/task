005aea5f919e4cd944db7989fd82eade5d47cb6f
nadersakr <nader150656@eng.psu.edu.eg>
2025-08-10 18:14:35 +0300

    Add SvgStrings class for SVG asset management

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,16 @@
+class SvgStrings{
+  static const String backgroundTriangle = "assets/images/svg/background_triangle.svg";
+
+  // products
+  static const String bike1 = "assets/images/svg/bike1.svg";
+  static const String bike2 = "assets/images/svg/bike2.svg";
+  static const String bike3 = "assets/images/svg/bike3.svg";
+  static const String helmet = "assets/images/svg/helmet.svg";
+
+  // icons
+  static const String bikeIcon = "assets/images/svg/icon_bike.svg";
+  static const String helmetIcon = "assets/images/svg/icon_helmet.svg";
+  static const String iconRoad = "assets/images/svg/icon_road.svg";
+  static const String iconMountain = "assets/images/svg/icon_mountain.svg";
+
+}
