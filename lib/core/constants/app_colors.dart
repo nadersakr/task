82194e846de3304ad59b7f05d2de820719a08d0b
nadersakr <nader150656@eng.psu.edu.eg>
2025-08-10 17:50:58 +0300

    Add files for Flutter application

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color background = const Color(0xFF242C3B);
+  static Color primary = const Color(0xFF37B6E9);
+  static Color secondary = const Color(0xFF4B4CED);
+  static Color tertiary = const Color(0xFF353F54);
+  static Color darker = const Color(0xFF222834);
+}
